6ada0b5533b19f5d48731a7046690e70e0c5bc87
doonemek <doonemek@gmail.com>
2026-04-27 14:37:15 +0900

    add: データクラス作成

---
+++ src/main/kotlin/net/home/server/DirectoryConfig.kt
@@ -0,0 +1,10 @@
+package net.home.server
+
+import java.io.File
+
+enum class DirectoryConfig(val path: String) {
+    // Dockerコンテナ内のプロジェクトルート配下のdataディレクトリ
+    DATA_ROOT("/workspaces/home-server-api/data");
+
+    val file: File get() = File(path)
+}
