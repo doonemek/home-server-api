267998dc5be7c5ccb8a622720497f1126a50f5d0
doonemek <doonemek@gmail.com>
2026-05-02 17:26:53 +0900

    refact: 設定変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
-    kotlin("plugin.serialization") version "2.0.0" 
+    kotlin("plugin.serialization") version "2.3.0"
 }
 
-group = "com.example"
+group = "net.home.server"
 version = "0.0.1"
 
 application {
