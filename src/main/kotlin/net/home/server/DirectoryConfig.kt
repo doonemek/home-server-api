c445f4275299968c5a17daae9b1c8331f4f2e786
doonemek <doonemek@gmail.com>
2026-05-04 13:52:09 +0900

    reverse: 通常に戻した

--- src/main/kotlin/net/home/server/DirectoryConfig.kt
+++ src/main/kotlin/net/home/server/DirectoryConfig.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 enum class DirectoryConfig(val path: String) {
     // Dockerコンテナ内のプロジェクトルート配下のdataディレクトリ
-    DATA_ROOT("/workspaces/home-server-api");
+    DATA_ROOT("/workspaces/home-server-api/data");
 
     val file: File get() = File(path)
 }
